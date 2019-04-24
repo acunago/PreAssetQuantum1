83e7ee7c5234cc5ba7eae2c12682af1b6e669141
acunago <49562365+acunago@users.noreply.github.com>
2019-04-23 23:51:29 -0300

    Cm2

---
+++ Assets/Script/SceneScripts/DeathFloorScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathFloorScript : MonoBehaviour
+{
+    public GameObject rtnPoint;
+    // Start is called before the first frame update
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.transform.position = rtnPoint.transform.position;
+    }
+}
