2b42a6e434962cfe8a43b1e7fc9a94f897f34f76
acunago <49562365+acunago@users.noreply.github.com>
2019-05-04 01:55:47 -0300

    Arreglo de caja

--- Assets/Script/SceneScripts/BoxScript.cs
+++ Assets/Script/SceneScripts/BoxScript.cs
@@ -15,6 +15,7 @@ public class BoxScript : MonoBehaviour
 
     public StateCaja state = StateCaja.PISO;
     public Animator animator;
+    public GameObject character;
     Rigidbody rb;
     
     private void Start()
@@ -25,17 +26,28 @@ public class BoxScript : MonoBehaviour
 
     private void Update()
     {
+        RaycastHit wallHit;
 
         if (state == StateCaja.AGARRADO)
         {
-
+            Debug.Log("va");
             if (Input.GetKey(KeyCode.W))
             {
 
                 rb.mass = 700;
+            }
+            Debug.DrawRay(character.transform.position, character.transform.forward * 10, Color.red);
 
+            float dot = Vector3.Dot(character.transform.forward, (transform.position - character.transform.position).normalized);
+            if (dot > 0.7f) { Debug.Log("Quite facing"); } else
+            {
+
+                rb.mass = 10000;
+                animator.SetBool("SoltarCaja", true);
+                state = StateCaja.PISO;
 
             }
+
         }
     }
     public void Agarre()
