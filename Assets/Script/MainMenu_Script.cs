0e0bd6d2bb86ba7c7ea601d107fa21af8afb82c5
FeerNeira <49598768+Feer98@users.noreply.github.com>
2019-05-22 12:28:21 -0300

    MainMenuScene

---
+++ Assets/Script/MainMenu_Script.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu_Script : MonoBehaviour
+{
+    public Transform currentPos, startPos, optionsPos, playPos;
+    public float speedFactor = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = Vector3.Lerp(transform.localPosition, currentPos.position, speedFactor);
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, currentPos.rotation, speedFactor);
+    }
+    
+    public void Move_1()
+    {
+        currentPos = startPos;
+    }
+
+    public void Move_2()
+    {
+        currentPos = optionsPos;
+    }
+
+    public void Move_3()
+    {
+        currentPos = playPos;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
