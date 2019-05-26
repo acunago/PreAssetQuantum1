90dfc64a7c75db88ebacfb218b9d0d85bf028eeb
acunago <49562365+acunago@users.noreply.github.com>
2019-05-26 15:19:17 -0300

    Arreglo de los saltitos

--- Assets/Script/SceneScripts/RockTrapScript.cs
+++ Assets/Script/SceneScripts/RockTrapScript.cs
@@ -6,19 +6,25 @@ public class RockTrapScript : MonoBehaviour
 {
     public GameObject trap;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.gameObject.layer == 8) { 
+            ActiveChild();
+        }
+    }
+
+    private void ActiveChild()
+    {
+        if (trap != null)
+        {
+            for (int i = 0; i < trap.transform.childCount; i++)
+            {
+                trap.transform.GetChild(i).GetComponent<Rigidbody>().useGravity = true;
+                trap.transform.GetChild(i).GetComponent<Rigidbody>().useGravity = true;
+                trap.transform.GetChild(i).GetComponent<Collider>().isTrigger = false;
+            }
+            trap.transform.GetComponent<Rigidbody>().useGravity = true;
+        }
     }
 }
