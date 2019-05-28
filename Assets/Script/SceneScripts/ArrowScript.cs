5b0ebae518fd680caaba9882dcbcb4ca97652f4d
acunago <49562365+acunago@users.noreply.github.com>
2019-05-27 23:38:45 -0300

    trampas

---
+++ Assets/Script/SceneScripts/ArrowScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ArrowState
+{
+    ACTIVE,
+    DISABLED
+}
+
+public class ArrowScript : MonoBehaviour
+{
+    public ArrowState state;
+    public GameObject Arrow;
+    public float destroyArrow = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = ArrowState.DISABLED;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(state == ArrowState.ACTIVE)
+        {
+            Arrow.transform.GetComponent<Rigidbody>().isKinematic = false;
+            Arrow.transform.GetComponent<Rigidbody>().AddForce(Arrow.transform.forward * 5);
+            Destroy(gameObject, destroyArrow);
+            Destroy(Arrow, destroyArrow);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+
+            state = ArrowState.ACTIVE;
+
+        }
+    }
+}
