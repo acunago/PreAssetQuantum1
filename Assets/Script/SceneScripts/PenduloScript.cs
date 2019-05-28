3655db3be13a1c298e0ead7b822bb302e33e2c7a
acunago <49562365+acunago@users.noreply.github.com>
2019-05-28 00:00:57 -0300

    cuchilla

---
+++ Assets/Script/SceneScripts/PenduloScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PenduloState
+{
+    ACTIVE,
+    DISABLED
+}
+
+public class PenduloScript : MonoBehaviour
+{
+    public PenduloState state;
+    private float i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = PenduloState.ACTIVE;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(state == PenduloState.ACTIVE)
+        {
+            i += Time.deltaTime;
+            transform.Rotate(new Vector3(0, 0,  Mathf.Sin(i)));
+
+        }
+        else
+        {
+            i = 0;
+        }
+    }
+}
