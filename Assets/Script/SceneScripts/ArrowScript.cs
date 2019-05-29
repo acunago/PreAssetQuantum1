5d262567c20be2cd8cb5f2b21954d6a05fbbbcdc
acunago <49562365+acunago@users.noreply.github.com>
2019-05-29 20:58:57 -0300

    Subida

--- Assets/Script/SceneScripts/ArrowScript.cs
+++ Assets/Script/SceneScripts/ArrowScript.cs
@@ -14,16 +14,10 @@ public class ArrowScript : MonoBehaviour
     public ArrowState state;
     public GameObject Arrow;
     public float destroyArrow = 5;
-    public float vel = 5;
-    public float time = 1;
-    public float auxTime;
-
     // Start is called before the first frame update
     void Start()
     {
-        Arrow.transform.GetChild(0).GetComponent<BoxCollider>().enabled = false;
         state = ArrowState.DISABLED;
-        auxTime = 0;
     }
 
     // Update is called once per frame
@@ -31,15 +25,10 @@ public class ArrowScript : MonoBehaviour
     {
       if(state == ArrowState.ACTIVE)
         {
-            auxTime += Time.deltaTime;
-            if (auxTime > 0.3f)
-            {
-                Arrow.transform.GetChild(0).GetComponent<BoxCollider>().enabled = true;
-                Arrow.transform.GetComponent<Rigidbody>().isKinematic = false;
-                Arrow.transform.GetComponent<Rigidbody>().AddForce(Arrow.transform.forward * vel);
-                Destroy(gameObject, destroyArrow);
-                Destroy(Arrow, destroyArrow);
-            }
+            Arrow.transform.GetComponent<Rigidbody>().isKinematic = false;
+            Arrow.transform.GetComponent<Rigidbody>().AddForce(Arrow.transform.forward * 5);
+            Destroy(gameObject, destroyArrow);
+            Destroy(Arrow, destroyArrow);
         }
     }
     private void OnTriggerEnter(Collider other)
---
+++ Assets/Script/SceneScripts/FireScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FireState
+{
+    ACTIVE,
+    DISABLED
+}
+
+public class FireScript : MonoBehaviour
+{
+    public FireState state;
+    public GameObject character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetActive()
+    {
+        if (state == FireState.ACTIVE)
+        {
+            state = FireState.DISABLED;
+        }
+        else
+        {
+            state = FireState.ACTIVE;
+        }
+
+    }
+}
