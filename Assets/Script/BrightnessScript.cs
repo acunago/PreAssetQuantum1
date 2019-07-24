d44ea87dc51406b9b799963151daee6ad5f55930
Ingenio1985 <facundo.tello@davinci.edu.ar>
2019-07-24 14:49:47 -0300

    Brightness + Acid

---
+++ Assets/Script/BrightnessScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrightnessScript : MonoBehaviour
+{
+    public void SetBrightness(float _value)
+    {
+        RenderSettings.ambientLight = new Color(_value, _value, _value, 1f);
+    }
+}
