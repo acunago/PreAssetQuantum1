bf1e7b3bd2e879fcc717c2ea76936ad45ff1850d
acunago <49562365+acunago@users.noreply.github.com>
2019-07-27 19:42:18 -0300

    Rocas

---
+++ Assets/Script/OnOffScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffScript : MonoBehaviour
+{
+    public List<GameObject> turnON;
+    public List<GameObject> turnOFF;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach (GameObject go in turnON)
+        {
+            go.SetActive(true);
+        }
+
+        foreach (GameObject go in turnOFF)
+        {
+            go.SetActive(false);
+        }
+
+        this.gameObject.SetActive(false);
+    }
+}
